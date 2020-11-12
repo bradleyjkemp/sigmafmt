fd851f9c58de5e6f14b222920aa9e60a599afd02
Bradley Kemp <bradleyjkemp@gmx.co.uk>
2020-11-12 18:09:54 +0000

    Fix relative paths

--- github-action/sigmafmt.sh
+++ github-action/sigmafmt.sh
@@ -3,12 +3,14 @@ set -e
 # Pre-download modules so the `go run` output is clean (doesn't include "downloading module X" lines)
 go mod download
 
-if [ -z "$INPUT_PATHS" ]
+if [ -z "$INPUT_PATH" ]
 then
-  INPUT_PATHS=$GITHUB_WORKSPACE
+  RULES_PATH=$GITHUB_WORKSPACE
+else
+  RULES_PATH=$GITHUB_WORKSPACE/$INPUT_PATH
 fi
 
-output=$(go run github.com/bradleyjkemp/sigmafmt -l -v $INPUT_PATHS)
+output=$(go run github.com/bradleyjkemp/sigmafmt -l -v "$RULES_PATH")
 
 if [ -z "$output" ]
 then
