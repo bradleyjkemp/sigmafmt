74127a70f5a3fb5548a2458c4511b53e6e2f7632
Bradley Kemp <bradleyjkemp@gmx.co.uk>
2020-11-12 15:30:19 +0000

    Polish

--- github-action/sigmafmt.sh
+++ github-action/sigmafmt.sh
@@ -1,6 +1,6 @@
 set -e
 
-# Pre-download modules so the `go run` output is clean
+# Pre-download modules so the `go run` output is clean (doesn't include "downloading module X" lines)
 go mod download
 
 output=$(go run github.com/bradleyjkemp/sigmafmt -l -v "$GITHUB_WORKSPACE")
@@ -11,5 +11,6 @@ then
 else
       echo "FAILURE: some files need re-formatting"
       echo "${output}"
+      # TODO: comment on the affected files in the PR
       exit 1
 fi
