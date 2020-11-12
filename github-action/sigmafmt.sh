744625f8bf4337f5725a1badddadf1c1d9af0e14
Bradley Kemp <bradleyjkemp@gmx.co.uk>
2020-11-12 18:02:57 +0000

    Allow formatting only specific paths via GitHub action

--- github-action/sigmafmt.sh
+++ github-action/sigmafmt.sh
@@ -3,14 +3,19 @@ set -e
 # Pre-download modules so the `go run` output is clean (doesn't include "downloading module X" lines)
 go mod download
 
-output=$(go run github.com/bradleyjkemp/sigmafmt -l -v "$GITHUB_WORKSPACE")
+if [ -z "$INPUT_PATHS" ]
+then
+  INPUT_PATHS=$GITHUB_WORKSPACE
+fi
+
+output=$(go run github.com/bradleyjkemp/sigmafmt -l -v $INPUT_PATHS)
 
 if [ -z "$output" ]
 then
-      echo "SUCCESS: all files formatted correctly"
+  echo "SUCCESS: all files formatted correctly"
 else
-      echo "FAILURE: some files need re-formatting"
-      echo "${output}"
-      # TODO: comment on the affected files in the PR
-      exit 1
+  echo "FAILURE: some files need re-formatting"
+  echo "${output}"
+  # TODO: comment on the affected files in the PR
+  exit 1
 fi
