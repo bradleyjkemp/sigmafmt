8c2c55cdb31109b3f455fa74485583fd25309f62
Bradley Kemp <bradleyjkemp@gmx.co.uk>
2022-03-14 10:18:44 +0000

    Auto-fix title punctuation and upper case

--- rules/rule_title.go
+++ rules/rule_title.go
@@ -9,36 +9,42 @@ import (
 )
 
 var ruleTitle = nodeRule{"rule_title_style", func(node *yaml.Node) ([]Message, error) {
-	var rule = struct {
-		Title string `yaml:"title"`
-	}{}
-	if err := node.Decode(&rule); err != nil {
-		return nil, err
+	var title *yaml.Node
+	for i := 1; i < len(node.Content); i += 2 {
+		key, value := node.Content[i-1], node.Content[i]
+		if key.Value == "title" {
+			title = value
+			break
+		}
 	}
-
-	if len(rule.Title) == 0 {
+	if title == nil || len(title.Value) == 0 {
+		// Rule doesn't have any title
 		return []Message{{Message: "All rules should have a title"}}, nil
 	}
 
 	var results []Message
-	if firstRune, _ := utf8.DecodeRuneInString(rule.Title); unicode.IsLower(firstRune) {
+	if firstRune, runeLen := utf8.DecodeRuneInString(title.Value); unicode.IsLower(firstRune) {
+		title.Value = string(unicode.ToUpper(firstRune)) + title.Value[runeLen:]
 		results = append(results, Message{
-			Message: "Rule titles should begin with an upper case letter",
+			AutoFixed: true,
+			Message:   "Rule titles should begin with an upper case letter",
 		})
 	}
-	if lastRune, _ := utf8.DecodeLastRuneInString(rule.Title); unicode.IsPunct(lastRune) {
+	if lastRune, runeLen := utf8.DecodeLastRuneInString(title.Value); unicode.IsPunct(lastRune) {
+		title.Value = title.Value[:len(title.Value)-runeLen]
 		results = append(results, Message{
-			Message: "Rule titles should not end in punctuation",
+			AutoFixed: true,
+			Message:   "Rule titles should not end in punctuation",
 		})
 	}
 
-	if len(rule.Title) > 120 {
+	if len(title.Value) > 120 {
 		results = append(results, Message{
 			Message: "Rule titles should be less than 120 characters so they can be used as an alert name. Leave explanation to the description field",
 		})
 	}
 
-	if strings.HasPrefix(rule.Title, "Detects ") || strings.HasPrefix(rule.Title, "Detect ") {
+	if strings.HasPrefix(title.Value, "Detects ") || strings.HasPrefix(title.Value, "Detect ") {
 		results = append(results, Message{
 			Message: "Don't use a prefix in the title like \"Detects\"",
 		})
